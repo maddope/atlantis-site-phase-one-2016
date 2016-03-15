67ea3a6b96887201fa0e837c39e99e2abbf17f06
Crystal Linly <crystal.linly@turner.com>
2016-03-15 18:22:43 -0400

    Added Omniture and COGIX code for tracking and sweeps and header/footer

---
+++ js/cn_omniture.js
@@ -0,0 +1,90 @@
+var omniTag = '201603_hasbroatlantis';
+
+function promoPageView(){
+    try {
+        trackMetrics({ 
+            type: "promo-main", 
+            data: ""
+        });
+    } catch (e) {
+    }
+}
+
+function promoClick(info){
+    try {
+        trackMetrics({ 
+            type: "promo-link", 
+            data: omniTag + " : " + info
+        });
+    } catch (e) {
+    }
+}
+
+function videoPlay(info){
+    try {
+		trackMetrics({
+			type: "video-start",
+			data: {
+				video: {
+					id: omniTag + " : " + info,
+					title: omniTag + ": " + info,
+					type: "promo"
+				}
+			}
+		});
+	}catch(e){}
+}
+
+function videoComplete(info){
+    try {
+		trackMetrics({
+			type: "video-complete",
+			data: {
+				video: {
+					id: omniTag + " : " + info,
+					title: omniTag + ": " + info,
+					type: "promo"
+				}
+			}
+		});
+	}catch(e){}
+}
+
+function videoJSPlay(info){
+    try {
+        trackMetrics({
+           type: "video-start",
+           data: {
+              video: {
+                 autoplayed: "false",
+                 id: omniTag + " : " + info,
+                 segment: "0",
+                 title: omniTag + " : " + info,
+                 type: "CLI",
+                 promo_video: omniTag + " : " + info
+              }
+            }			 
+        });
+    } catch(e){
+    }
+}
+
+function videoJSComplete(info){
+    try {
+        trackMetrics({
+           type: "video-complete",
+           data: {
+              video: {
+                 autoplayed: "false",
+                 id: omniTag + " : " + info,
+                 segment: "0",
+                 title: omniTag + " : " + info,
+                 type: "CLI",
+                 promo_video:  omniTag + " : " + info
+              }
+            }			 
+        });
+    } catch(e){
+    }
+}
+
---
+++ js/cogix.js
@@ -0,0 +1,41 @@
+var ValidatorArray = new Array (); // holds validation statements to be evaluated in OnSubmit
+var ValidatorArrayProperties = new Array();
+ValidatorArrayProperties.ValidatorSuppress = false;  // if true, skip onSubmit validation
+
+var i18nArray = new Array ();   //  hold translated strings; addressed as i18nArray('msgid');
+//i18nArray['requiredanswer'] = "Please answer the question. If not sure of what question this refers to, press the space bar.";
+i18nArray['requiredanswer'] = "Whoops! You forgot to finish the form!";
+i18nArray['entervaluebetween'] = "Enter a value between %1 and %2";
+i18nArray['entervaluenogreater'] = "Enter a value of %2 or below";
+i18nArray['entervaluenoless'] = "Enter a value of %1 or above";
+i18nArray['checknumbermsg'] = "Enter a number in format %1";
+i18nArray['checkvaliddatemsg'] = "Use only numbers in dates, in format %X|%1 must be between %2 and %3; format is %X|Day|Month|Year|m/d/y";
+i18nArray['checkvalidemailmsg1'] = "Two @s not allowed in an E-mail address.";
+i18nArray['checkvalidemailmsg2'] = "The character '%1' is not allowed in an E-mail address.";
+i18nArray['checkvalidemailmsg3'] = "@ missing from E-mail address.";
+i18nArray['checkvalidemailmsg4'] = "E-Mail address cannot start with @.";
+i18nArray['checkvalidemailmsg5'] = "E-mail domain name is missing a period.";
+i18nArray['checkvalidemailmsg6'] = "E-mail domain name can't start with a period.";
+i18nArray['checkvalidemailmsg7'] = "E-mail domain name can't end with a period.";
+
+
+
+function onSubmitCartoonNetworkAtlantis2016() {
+	promoClick('sweeps submit button clicked');
+    return vwfOnSubmit();
+    }
+
+function qhelperCartoonNetworkAtlantis2016(d) {
+    return vwfQhelper(d);   //  located in Standard_.js
+    }
+
+function qlengthcheckerCartoonNetworkAtlantis2016(a,b) {
+    return vwfQlengthchecker(a,b);  //  located in Standard_.js
+    }
+
+var checkexpressionemail_address = "vwfCheckEmail ( document.forms['formCartoonNetworkAtlantis2016'].elements['email_address']) ";
+var checkansweredexpressionemail_address = "checkQuestionAnswered(document.forms['formCartoonNetworkAtlantis2016'].elements['email_address'],' checked','','','0')";
+
+ValidatorArray [ ValidatorArray.length ] = checkexpressionemail_address;
+ValidatorArray [ ValidatorArray.length ] = checkansweredexpressionemail_address;
+
